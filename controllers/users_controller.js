32f0bb7446694c87a7beb52c1d0e42e82206f34c
yash ahire <yashahahire1234@gmail.com>
2023-10-13 14:02:17 +0530

    set the view engine as ejs and perform rendering

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function(req,res){
-    res.end("<h1>Users Profile!!!</h1>");
+    // res.end("<h1>Users Profile!!!</h1>");
+
+    return res.render('users.ejs',{
+        title : "Users page"
+    });
 }
